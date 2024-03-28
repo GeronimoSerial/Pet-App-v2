c988fe915d0482c1820a6ad300fa895b032749dd
Geronimo Serial <serialgeronimo@gmail.com>
2024-03-28 18:35:12 -0300

    New components for Home

---
+++ src/app/pages/tabs-pages/my-pets/my-pets.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-pets',
+  templateUrl: './my-pets.component.html',
+  styleUrl: './my-pets.component.scss'
+})
+export class MyPetsComponent {
+
+}
