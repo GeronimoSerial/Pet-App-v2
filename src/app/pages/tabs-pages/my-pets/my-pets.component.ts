d68e8b0df3b53e34893b0a88d02746b7fbe48d6f
Geronimo <serialgeronimo@gmail.com>
2024-04-06 09:48:12 -0300

    fix responsive design

--- src/app/pages/tabs-pages/my-pets/my-pets.component.ts
+++ src/app/pages/tabs-pages/my-pets/my-pets.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PetsInterface } from '../../../core/models/pet';
 
 @Component({
   selector: 'app-my-pets',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './my-pets.component.scss'
 })
 export class MyPetsComponent {
-
+  data: PetsInterface[] = [];
+  pets: PetsInterface[] = []; 
+  constructor() {
+    this.pets = []
+    for (let i = 0; i < 50; i++) {
+      var harcodePet: PetsInterface = {
+        id: i,
+        name: 'Pet' + i,
+        type: 'Dog',
+        breed: 'Breed' + i,
+        birthday: new Date(),
+        imageUrl: new FormData(),
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto sint at? Nostrum consequuntur accusamus, eveniet dolorem voluptatibus laboriosam libero voluptate, similique perferendis voluptas magni consectetur quia quam incidunt repellat         ' + i,
+        userId: i,
+      }
+      this.pets.push(harcodePet);
+    }
+  }
 }
