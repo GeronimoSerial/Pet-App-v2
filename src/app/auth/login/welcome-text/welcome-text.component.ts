98673891baeb93ec791b5b98a4ed4380819929ae
Geronimo <serialgeronimo@gmail.com>
2024-04-08 18:29:48 -0300

    added animated welcome-text

---
+++ src/app/auth/login/welcome-text/welcome-text.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-text',
+  templateUrl: './welcome-text.component.html',
+  styleUrls: ['./welcome-text.component.scss']
+})
+export class WelcomeTextComponent implements OnInit {
+
+  words = ['owners ❌', 'parents ✔️', 'lovers 😍', 'fans !🤩'];
+  current = 0;
+  fadeOut = false;
+  fadeIn = false;
+  // @ViewChild('changingWord', { static: true }) changingWord: ElementRef;
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.fadeOut = true;
+      setTimeout(() => {
+        this.current = (this.current + 1) % this.words.length;
+        this.fadeOut = false;
+        this.fadeIn = true;
+        setTimeout(() => {
+          this.fadeIn = false;
+        }, 1000);
+      }, 1000);
+    }, 3000);
+  }
+}
+
